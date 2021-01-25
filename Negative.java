dbcb12a854ceac4faf4d6120e98f1a04afaa931f
InsideIntel <50695683+InsideIntel@users.noreply.github.com>
2021-01-25 13:26:57 -0600

    Update Negative.java

--- Negative.java
+++ Negative.java
@@ -1,8 +1,7 @@
 public class Negative {
 
   public static int makeNegative(final int x) {
-       // return String.valueOf(x).contains("-") ? x : Integer.parseInt("-" + x);
-    return -Math.abs(x);
+       return String.valueOf(x).contains("-") ? x : Integer.parseInt("-" + x);
     }
   
 }
